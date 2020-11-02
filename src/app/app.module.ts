73d180ad2de8c8cc7da96bad0c32a75bcde064eb
Liju Kuriakose <lijukuriakose@Lijus-MacBook-Pro.local>
2020-11-02 12:58:16 +0530

    fix: Adding support for localization in english and hindi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { FormsModule } from '@angular/forms'
 import { ReactiveFormsModule} from '@angular/forms' 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 
 import { ColorPickerModule } from 'ngx-color-picker';
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
 import {ListPageModule} from  './list-page/list-page.module';
 
+
+// import ngx-translate and the http loader
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
 @NgModule({
   declarations: [
     AppComponent,
-    
   ],
   imports: [
     CommonModule,
@@ -25,7 +29,15 @@ import {ListPageModule} from  './list-page/list-page.module';
     HttpClientModule,
     FormsModule, 
     ListPageModule,
-    ColorPickerModule
+    ColorPickerModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  })
+
   ],
   providers: [
     fakeBackendProvider
@@ -33,3 +45,7 @@ import {ListPageModule} from  './list-page/list-page.module';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
